e188195f44ac040eeceae9067e4006d64c47c36c
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-22 14:39:01 +0530

    (Shubham)Add: My practice employee attendance problem

---
+++ Practice1/EmpPractice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Practice1
+{
+    public class EmpPractice
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
---
+++ Practice1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ask us what you want to know");
+            EmpPractice emp = new EmpPractice();
+            emp.Attendance();
+
+        }
+    }
+}

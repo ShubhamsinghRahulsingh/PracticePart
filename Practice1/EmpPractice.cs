e4f25c19f1852a9d7fc4990ec817290176ddc773
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-26 12:20:23 +0530

    (Shubham)Add: Practicing adding Part time employee and wage

--- Practice1/EmpPractice.cs
+++ Practice1/EmpPractice.cs
@@ -4,6 +4,7 @@ namespace Practice1
     public class EmpPractice
     {
         const int WAGE_PER_HR = 20;
+        int IS_FULL_TIME = 1, IS_PART_TIME = 2;
         Random random = new Random();
         public void Attendance()
         {
@@ -19,14 +20,28 @@ namespace Practice1
         }
         public void EmpWage()
         {
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             int empHrs = 0, dailyWage = 0;
-            if (empCheck == 0)
+            if (empCheck == 1)
+            {
                 empHrs = 8;
+                Console.WriteLine("Employee is present for full day");
+            }
+            else if (empCheck == 2)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is present for Half day");
+
+            }
             else
+            {
                 empHrs = 0;
+                Console.WriteLine("Employee is Absent");
+            }
             dailyWage = empHrs * WAGE_PER_HR;
-            Console.WriteLine("Daily wage of employee is:{0}",dailyWage);
+            Console.WriteLine("Daily wage of employee is " + dailyWage);
+            
+
 
         }
     }

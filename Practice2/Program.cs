8b2ddec31281b55b2012cce7bab404b642d5d072
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-27 12:39:54 +0530

    (Shubham)Add: Calculating Length

---
+++ Practice2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line Comparison problem statement");
+            LinePractice length = new LinePractice(1, 7, 3, 7);
+            length.Calculate();
+        }
+    }
+}
+
